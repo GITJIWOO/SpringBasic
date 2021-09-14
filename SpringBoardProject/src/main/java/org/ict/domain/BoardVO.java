15a4633cba7284a73e3d1cb5db3308d68b17c58c
GITJIWOO <kjw011231@gmail.com>
2021-09-14 13:24:56 +0900

    modify

--- SpringBoardProject/src/main/java/org/ict/domain/BoardVO.java
+++ SpringBoardProject/src/main/java/org/ict/domain/BoardVO.java
@@ -8,6 +8,7 @@ import lombok.Data;
 public class BoardVO {
 	private Long bno;
 	private String title;
+	// 시간은 Date로 작성합니다. java.sql 내부 자료입니다.
 	private String content;
 	private String writer;
 	private Date regdate;
